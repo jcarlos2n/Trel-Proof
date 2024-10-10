78e4a903953f8455b775d7f2c6711ab1e46c3feb
jcarlos2n <eljcarlos2n@gmail.com>
2024-10-10 12:22:10 -0400

    config of prisma scheme, create signup page

---
+++ app/components/header.tsx
@@ -0,0 +1,13 @@
+  
+  export default function Header() {
+    return (
+      <div className="lg:flex lg:items-center items-center bg-slate-900">
+        <div className="min-w-0 flex-1">
+          <h2 className="text-2xl font-bold leading-7 text-brand-aqua sm:truncate sm:text-3xl sm:tracking-tight">
+            TREL-PROOF
+          </h2>
+        </div>
+      </div>
+    )
+  }
+  
